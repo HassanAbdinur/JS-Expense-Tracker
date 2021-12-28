baccd72a7a0523291df57d5539513044cd78ff3d
Hassan Abdinur <hassan.abdinur123@gmail.com>
2021-12-27 18:24:50 -0800

    debugging

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-document.getElementById('submitData').onclick = function() {
-    
+document.getElementById('submit').onclick = function () {
+    document.getElementById('table').style.display = "block";
+
+    var table = document.getElementById('table');
+    var row = table.insertRow(-1);
+    var expense = row.insertCell(0);
+    var amount = row.insertCell(1);
+    var date = row.insertCell(2);
+
+    expense.innerHTML = document.getElementById('expense');
+    amount.innerHTML = document.getElementById('amount');
+    date.innerHTML = document.getElementById('date');
+
+    return false;
 }
