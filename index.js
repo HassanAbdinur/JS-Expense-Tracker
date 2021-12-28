f9d839cae0d906eb06046505f6cb1e4b2098e021
Hassan Abdinur <hassan.abdinur123@gmail.com>
2021-12-27 22:16:22 -0800

    adding expense function

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ var expense_data = document.getElementById('expense-data');
 var amount_data = document.getElementById('amount-data');
 var date_data = document.getElementById('date-data');
 
+function expenseLength() {
+    return expense.value.length;
+}
+
