5fb97aec3c6c4b5e3e5b73619d105631efbd149a
Hassan Abdinur <hassan.abdinur123@gmail.com>
2021-12-27 22:14:56 -0800

    added variables

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+var expense = document.getElementById('expense');
+var amount = document.getElementById('amount');
+var date = document.getElementById('date');
+
+var expense_data = document.getElementById('expense-data');
+var amount_data = document.getElementById('amount-data');
+var date_data = document.getElementById('date-data');
 
