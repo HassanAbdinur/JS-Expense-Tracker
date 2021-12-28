03969a9a0798f45d4216b908f32571bd64803789
Hassan Abdinur <hassan.abdinur123@gmail.com>
2021-12-27 22:11:22 -0800

    rewriting file

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-document.getElementById('submit').onclick = function () {
-    document.getElementById('table').style.display = "block";
 
-    var table = document.getElementById('table');
-    var row = table.insertRow(-1);
-    var expense = row.insertCell(0);
-    var amount = row.insertCell(1);
-    var date = row.insertCell(2);
-
-    expense.innerHTML = document.getElementById('expense').value;
-    amount.innerHTML = document.getElementById('amount').value;
-    date.innerHTML = document.getElementById('date').value;
-
-
-    return false;
-}
