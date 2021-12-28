ce8dcc2c3b7cb9ed7ca082ff915dfde6f2638067
Hassan Abdinur <hassan.abdinur123@gmail.com>
2021-12-27 16:42:41 -0800

    editing file

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var expense = document.getElementById('expense');
-var amount = document.getElementById('amount');
-var expense_date = document.getElementById('expense-date');
-
+document.getElementById('submitData').onclick = function() {
+    
+}
